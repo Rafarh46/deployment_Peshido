e981e3198fbbf526023bb9c5bae44b91264e5dc2
Rafael <romerorh46@gmail.com>
2024-02-29 18:04:44 -0600

    change

--- src/components/modules/index/Hero.js
+++ src/components/modules/index/Hero.js
@@ -111,7 +111,7 @@ const Hero = () => {
                     <li className="social__item">
                       <Link
                         scroll={false} href="https://t.me/peshido"
-                        className="social__link social__link--style2"
+                        className="social__link social__link--style1"
                       >
                         <i className="fab fa-telegram"></i>
                       </Link>
@@ -120,7 +120,7 @@ const Hero = () => {
                     <li className="social__item">
                       <Link
                         href="https://twitter.com/PeshidoToken"
-                        className="social__link social__link--style3"
+                        className="social__link social__link--style1"
                       >
                         <i className="fab fa-twitter"></i>
                       </Link>
